5243411e4fe825585f2d3eb3d88e9f1aa12e2e09
Anonimous05 <airastolonov05@gmail.com>
2022-12-13 20:13:27 +0600

    lesson 2

---
+++ posts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from posts.models import Post
+
+# Create your views here.
+
+
+def main_view(request):
+    return render(request, 'layouts/index.html')
+
+
+def posts_view(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+
+        return render(request, 'posts/posts.html', context={
+            'posts': posts
+        })
+
